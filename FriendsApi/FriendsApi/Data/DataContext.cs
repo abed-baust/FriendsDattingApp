e54b51641cc7954312f6951b45fd368e605fe96b
Abedur Rahman <abedur@bitmascot.com>
2022-02-18 16:51:21 +0600

    Database Connection | Model of Demo User

---
+++ FriendsApi/FriendsApi/Data/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using FriendsApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FriendsApi.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options):base(options)
+        {
+        }
+
+        public DbSet<AppUser> Users { get; set; }
+    }
+}
