cff15396fa9237f046c3258a5c9250846b175237
Abedur Rahman <abedur@bitmascot.com>
2022-02-19 01:39:55 +0600

    User model added and database created with test data ..

---
+++ FriendsApi/FriendsApi/Controllers/UsersController.cs
@@ -0,0 +1,35 @@
+﻿using FriendsApi.Data;
+using FriendsApi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FriendsApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _context;
+        public UsersController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<AppUser>>> GetUsers()
+        {
+            return await _context.Users.ToListAsync();
+        }
+
+        //api//users/3
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AppUser>> GetUser(int id)
+        {
+            return await _context.Users.FindAsync(id);
+        }
+    }
+}
