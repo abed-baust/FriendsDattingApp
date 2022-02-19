08fff2666087362af9093f9bcde107aea558a478
Abedur Rahman <abedur@bitmascot.com>
2022-02-19 13:51:00 +0600

    Cross-origin resource sharing(Cors) Added initial for vue

--- FriendsApi/FriendsApi/Startup.cs
+++ FriendsApi/FriendsApi/Startup.cs
@@ -35,7 +35,7 @@ namespace FriendsApi
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "FriendsApi", Version = "v1" });
             });
-            
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -51,6 +51,8 @@ namespace FriendsApi
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors(x =>x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:8080"));
 
             app.UseAuthorization();
 
