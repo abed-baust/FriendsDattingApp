fb46332cecb3a52853ea051213c3a0f91aa79aa6
Abedur Rahman <abedur@bitmascot.com>
2022-03-18 16:52:00 +0600

    updating the users controller

---
+++ FriendsApi/FriendsApi/Data/UserRepository.cs
@@ -0,0 +1,43 @@
+﻿using FriendsApi.Interface;
+using FriendsApi.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace FriendsApi.Data
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly DataContext _context;
+
+        public UserRepository(DataContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<AppUser> GetUserByIdAsync(int id)
+        {
+            return await _context.Users.FindAsync(id);
+        }
+
+        public async Task<AppUser> GetUserByNameAsync(string reqUserName)
+        {
+            return await _context.Users.SingleOrDefaultAsync(x => x.userName == reqUserName);
+        }
+
+        public async Task<IEnumerable<AppUser>> GetUsersAsync()
+        {
+            return await _context.Users.ToListAsync();
+        }
+
+        public async Task<bool> SaveAllAsync()
+        {
+            return await _context.SaveChangesAsync() > 0;
+        }
+
+        public void Update(AppUser user)
+        {
+            _context.Entry(user).State = EntityState.Modified;
+        }
+    }
+}
---
+++ FriendsApi/FriendsApi/Interface/IUserRepository.cs
@@ -0,0 +1,15 @@
+﻿using FriendsApi.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace FriendsApi.Interface
+{
+    public interface IUserRepository
+    {
+        void Update(AppUser user);
+        Task<bool> SaveAllAsync();
+        Task<IEnumerable<AppUser>> GetUsersAsync();
+        Task<AppUser> GetUserByIdAsync(int id);
+        Task<AppUser> GetUserByNameAsync(string userName); 
+    }
+}
