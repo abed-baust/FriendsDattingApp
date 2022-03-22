f847f184dd999dcbc9d073bbe4af89d9217e93f8
Abedur Rahman <abedur@bitmascot.com>
2022-03-23 00:03:32 +0600

    Update User

--- FriendsApi/FriendsApi/Controllers/UsersController.cs
+++ FriendsApi/FriendsApi/Controllers/UsersController.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace FriendsApi.Controllers
@@ -38,7 +39,19 @@ namespace FriendsApi.Controllers
         public async Task<ActionResult<MemberDto>> GetUser(string userName)
         {
             return await _userRepository.GetMemberAsync(userName);
-           
+        }
+
+        [HttpPut]
+        [AllowAnonymous]
+        public async Task<ActionResult> UpdateUser(MemberUpdateDto memberUpdateDto)
+        {
+            var userName = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            var user = await _userRepository.GetUserByNameAsync(userName);
+            _mapper.Map(memberUpdateDto, user);
+            _userRepository.Update(user);
+
+            if (await _userRepository.SaveAllAsync()) return NoContent();
+            return BadRequest("Failed to Update user!");
         }
     }
 }
