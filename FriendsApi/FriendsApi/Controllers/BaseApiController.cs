050f8ed37039fc7709475c9efcd21c9d9240f07e
Abedur Rahman <abed.baust@gmail.com>
2022-04-15 18:09:00 +0600

    162 Adding an action filter

--- FriendsApi/FriendsApi/Controllers/BaseApiController.cs
+++ FriendsApi/FriendsApi/Controllers/BaseApiController.cs
@@ -1,8 +1,10 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using FriendsApi.Helpers;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FriendsApi.Controllers
 {
+    [ServiceFilter(typeof(LogUserActivity))]
     [Route("api/[controller]")]
     [ApiController]
     public class BaseApiController : ControllerBase
---
+++ FriendsApi/FriendsApi/Helpers/LogUserActivity.cs
@@ -0,0 +1,26 @@
+﻿using FriendsApi.Extensions;
+using FriendsApi.Interface;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Threading.Tasks;
+
+namespace FriendsApi.Helpers
+{
+    public class LogUserActivity : IAsyncActionFilter
+    {
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+           var resultContext = await next();
+           if (!resultContext.HttpContext.User.Identity.IsAuthenticated) return;
+
+            var userName = resultContext.HttpContext.User.GetUserName();
+
+            var repo = resultContext.HttpContext.RequestServices.GetService<IUserRepository>();
+
+            var user = await repo.GetUserByNameAsync(userName);
+            user.LastActive = DateTime.Now;
+            await repo.SaveAllAsync();
+        }
+    }
+}
