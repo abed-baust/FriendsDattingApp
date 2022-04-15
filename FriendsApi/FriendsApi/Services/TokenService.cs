38d4e81742351b6680ba49ae7a6bb92e1cfa6db9
Abedur Rahman <abed.baust@gmail.com>
2022-04-15 18:50:50 +0600

    163 Making the Last Active action filter more optimal

--- FriendsApi/FriendsApi/Services/TokenService.cs
+++ FriendsApi/FriendsApi/Services/TokenService.cs
@@ -22,7 +22,8 @@ namespace FriendsApi.Services
         {
             var claims = new List<Claim>
            {
-               new Claim(JwtRegisteredClaimNames.NameId,user.userName)
+               new Claim(JwtRegisteredClaimNames.NameId,user.Id.ToString()),
+               new Claim(JwtRegisteredClaimNames.UniqueName,user.userName),
            };
             var creds = new SigningCredentials(_key, SecurityAlgorithms.HmacSha256Signature);
 
