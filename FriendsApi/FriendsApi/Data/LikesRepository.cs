b9646c1c84c57ada84f5d484f36b2927bdea1d6e
Abedur Rahman <abed.baust@gmail.com>
2022-04-16 23:41:50 +0600

    172 Adding a likes repository

---
+++ FriendsApi/FriendsApi/Data/LikesRepository.cs
@@ -0,0 +1,32 @@
+﻿using FriendsApi.DTOs;
+using FriendsApi.Interface;
+using FriendsApi.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace FriendsApi.Data
+{
+    public class LikesRepository : ILikesRepository
+    {
+        private readonly DataContext _context;
+        public LikesRepository(DataContext context)
+        {
+            _context = context;
+        }
+
+        public Task<UserLike> GetUserLike(int sourceUserId, int likedUserId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<IEnumerable<LikeDto>> GetUserLikes(string predicate, int userId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<AppUser> GetUserWithLikes(int userId)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ FriendsApi/FriendsApi/Interface/ILikesRepository.cs
@@ -0,0 +1,14 @@
+﻿using FriendsApi.DTOs;
+using FriendsApi.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace FriendsApi.Interface
+{
+    public interface ILikesRepository
+    {
+        Task<UserLike> GetUserLike(int sourceUserId, int likedUserId);
+        Task<AppUser> GetUserWithLikes(int userId);
+        Task<IEnumerable<LikeDto>> GetUserLikes(string predicate, int userId);
+    }
+}
