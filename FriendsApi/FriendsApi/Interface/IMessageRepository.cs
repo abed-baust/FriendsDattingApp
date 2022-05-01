d464d2638cd79c0a9eca2d377d2248f080e93936
Abedur Rahman <abed.baust@gmail.com>
2022-05-02 00:51:59 +0600

    183 Setting up the automapper profiles

---
+++ FriendsApi/FriendsApi/Interface/IMessageRepository.cs
@@ -0,0 +1,19 @@
+﻿using FriendsApi.DTOs;
+using FriendsApi.Helpers;
+using FriendsApi.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace FriendsApi.Interface
+{
+    public interface IMessageRepository
+    {
+        void AddMessage(Message message);
+        void DeleteMessage (Message message);
+        Task<Message> GetMessage(int id);
+        Task<PagedList<MessageDto>> GetMessagesForUser();
+        Task<IEnumerable<MessageDto>> GetMessageThread(int currentUserId, int recipientId);
+        Task<bool> SaveAllAsync();
+
+    }
+}
